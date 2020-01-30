5aea959ab14d885f4c5ed5738e96cc3b33b368dc
Gemmafj <59705270+Gemmafj@users.noreply.github.com>
2020-01-30 18:11:21 +0100

    Fitxers modificats Activitat 2 GemmaFita30/01/2020
    2html js i odt

---
+++ codijs.js
@@ -0,0 +1,7 @@
+var elmeunom ="Hola Gemma";
+    alert (elmeunom);
+var segonmissatge = "Sóc el segon missatge!";
+    alert (segonmissatge);
+var comentari="Hem fet un fitxewr independent de js i lhem anllaçat a aquesta pàgina">
+    alert(comentari);
+    
