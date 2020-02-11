257f154ba41d3fccdc2e18e570ff83d67c351333
Gemmafj <59705270+Gemmafj@users.noreply.github.com>
2020-02-11 15:59:57 +0100

    Activitat 2 Xavi ENTREGADA
    Act 2 entregada

---
+++ Activitat 2 Xavi/xcodijs.js
@@ -0,0 +1,5 @@
+
+var elmeunom ="Gemma Fita Julià";
+    alert (elmeunom); /*Aquesta variable fa que s'obri un alert dient el meu nom*/
+var segonmissatge = "Sóc el segon missatge!";
+    alert (segonmissatge); /*Un cop cliquem el "d'acord" del primer alert, s'obre aquesta segona alerta, la qual també haurem de donar l'ok si volem seguir endavant*/ 
