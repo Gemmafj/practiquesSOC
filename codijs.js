20fd5e9720fb8cdf9451c4a025fe689a6b15bcb7
Gemmafj <59705270+Gemmafj@users.noreply.github.com>
2020-02-11 16:01:46 +0100

    borrar codijs.js

--- codijs.js
+++
@@ -1,7 +0,0 @@
-var elmeunom ="Hola Gemma";
-    alert (elmeunom);
-var segonmissatge = "Sóc el segon missatge!";
-    alert (segonmissatge);
-var comentari="Hem fet un fitxewr independent de js i lhem anllaçat a aquesta pàgina">
-    alert(comentari);
-    
