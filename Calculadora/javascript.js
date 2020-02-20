18c082c1cc7c677ade67e40bcd408f8cd32f8e49
Gemmafj <59705270+Gemmafj@users.noreply.github.com>
2020-02-20 23:50:26 +0100

    Calculadora
    Calculadora html, css i js

---
+++ Calculadora/javascript.js
@@ -0,0 +1,14 @@
+
+var cifra="";
+var acumulado=0;
+
+function display_numeros(numero) {
+  document.getElementById("display").value=cifra+numero;
+cifra=document.getElementById("display").value;
+}
+
+function suma() {
+  acumulado=acumulado+parseInt(cifra);
+  document.getElementById("display").value=acumulado;
+  cifra="";
+}
