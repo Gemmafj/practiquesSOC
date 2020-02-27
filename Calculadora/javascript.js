c1826fd09ac8f799122ab1cd055af8b2f965ebfc
Gemmafj <59705270+Gemmafj@users.noreply.github.com>
2020-02-27 23:11:43 +0100

    27/02/2020

--- Calculadora/javascript.js
+++ Calculadora/javascript.js
@@ -1,14 +1,54 @@
 
 var cifra="";
 var acumulado=0;
+var sumar=false;
+var restar=false;
+var p_operacion=true;
+/*var multiplicar=false;
+var dividir=false;*/
 
-function display_numeros(numero) {
+function display_numeros(numero) { /// funcio per concatenar sense sumar
   document.getElementById("display").value=cifra+numero;
 cifra=document.getElementById("display").value;
 }
 
+/*window.onload=function() {
+  this.document.getElementById("display").value=0
+}*/
+
 function suma() {
-  acumulado=acumulado+parseInt(cifra);
+  if (restar) {
+    acumulado=acumulado-parseInt(cifra);
   document.getElementById("display").value=acumulado;
+  }
+  else{
+    acumulado=acumulado+parseInt(cifra);
+  document.getElementById("display").value=acumulado;
+  }
   cifra="";
+  sumar=true;
+  p_operacion=false;
+}
+
+
+function resta() {
+  if (sumar) {
+    acumulado=acumulado+parseInt(cifra);
+    document.getElementById("display").value=acumulado;
+  }else{
+  acumulado=acumulado-parseInt(cifra);
+  document.getElementById("display").value=acumulado;}
+
+  cifra="";
+  sumar=false;
+  restar=true;
+}
+
+function resultado() {
+  if (sumar) {
+    document.getElementById("display").value=acumulado+parseInt(cifra);
+  } else if(restar){
+    document.getElementById("display").value=acumulado-parseInt(cifra);
+  }
+  
 }
